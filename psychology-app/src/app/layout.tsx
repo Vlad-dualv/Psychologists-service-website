1f8fafba0256eca776cfee2c34cb0a7c63e8c33e
Vlad <vladswork95@gmail.com>
2025-09-15 10:22:52 +0100

    Proj init

---
+++ psychology-app/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Psychologists",
+  description: "Psychologists directory and booking app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
