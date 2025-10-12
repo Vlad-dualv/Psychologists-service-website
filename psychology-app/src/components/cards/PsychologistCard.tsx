222b2dd25bec208d1fa1114393854a0339f18209
Vlad <vladswork95@gmail.com>
2025-10-12 12:46:49 +0100

    style: mobile PsychologistCard

--- psychology-app/src/components/cards/PsychologistCard.tsx
+++ psychology-app/src/components/cards/PsychologistCard.tsx
@@ -34,7 +34,7 @@ export default function PsychologistCard({
   const imageUrl = "https://ftp.goit.study/img/avatars/23.jpg";
 
   return (
-    <div className="bg-white rounded-[30px] p-6 flex flex-col md:flex-row gap-4 md:gap-6 tracking-normal">
+    <div className="bg-white rounded-[30px] p-6 flex flex-col md:flex-row gap-4 md:gap-6 tracking-normal max-md:text-sm relative">
       <div>
         <div className="relative">
           <Image
@@ -47,44 +47,54 @@ export default function PsychologistCard({
           <div className="bg-[rgba(56,205,62,1)] w-[10px] h-[10px] rounded-full absolute top-2 left-20 border-2 border-white"></div>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <div>
-          <p className="text-gray-500 font-medium">Psychologist</p>
-          <div className="flex items-center gap-1">
-            <Star size={16} />
-            <p>Rating: 4.75</p>
+      <div className="flex flex-col gap-4 md:gap-6">
+        <div className="font-medium">
+          <p className="text-gray-500 mb-2">Psychologist</p>
+          <div className="flex gap-4">
+            <div className="flex items-center gap-1 border-r-[1px] border-gray-200 pr-2">
+              <Star size={16} className="fill-brand-yellow text-transparent" />
+              <p>Rating: 4.75</p>
+            </div>
+            <div>
+              <p>
+                Price / 1 hour:{" "}
+                <span className="text-[rgba(56,205,62,1)]">120$</span>
+              </p>
+            </div>
           </div>
-          <div>
-            <p>
-              Price / 1 hour:{" "}
-              <span className="text-[rgba(56,205,62,1)]">120$</span>
-            </p>
-          </div>
-          <Heart size={23} />
+          <button
+            type="button"
+            className={`absolute top-4 right-4 ${
+              isFavorite ? "fill-inherit" : "fill-brand-green"
+            }`}
+            onClick={handleFavoriteClick}
+          >
+            <Heart size={23} />
+          </button>
         </div>
-        <h3>Dr. Sarah Davis</h3>
-        <div>
-          <p className="text-gray-500">
+        <h3 className="text-lg font-medium">Dr. Sarah Davis</h3>
+        <div className="font-medium flex flex-wrap gap-1">
+          <p className="inline-block text-gray-500 bg-[rgba(243,243,243,1)] py-2 px-4 rounded-[24px]">
             Experience: <span className="text-black">12 years</span>
           </p>
-          <p className="text-gray-500">
+          <p className="inline-block text-gray-500 bg-[rgba(243,243,243,1)] py-2 px-4 rounded-[24px]">
             Licence:{" "}
             <span className="text-black">
               Licensed Psychologist (Licence #67890)
             </span>
           </p>
-          <p className="text-gray-500">
+          <p className="inline-block text-gray-500 bg-[rgba(243,243,243,1)] py-2 px-4 rounded-[24px]">
             Specialization:{" "}
             <span className="text-black">Cognitive Behavioral Therapy</span>
           </p>
-          <p className="text-gray-500">
+          <p className="inline-block text-gray-500 bg-[rgba(243,243,243,1)] py-2 px-4 rounded-[24px]">
             Initial consultation:{" "}
             <span className="text-black">
               Free 45-minute initial consultation
             </span>
           </p>
         </div>
-        <p className="text-gray-400">
+        <p className="max-md:hidden text-gray-400 text-sm">
           Dr. Sarah Davis is a highly experienced and licensed psychologist
           specializing in Depression and Mood Disorders. With 12 years of
           practice, she has helped numerous individuals overcome their
