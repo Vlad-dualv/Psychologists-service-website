6ed4897cf370ab17327d1ff3cd1b47e87ce09029
Vlad <vladswork95@gmail.com>
2025-10-25 12:44:26 +0100

    FAvorites page init

--- psychology-app/src/app/favorites/page.tsx
+++ psychology-app/src/app/favorites/page.tsx
@@ -5,10 +5,13 @@ import { useFavorites } from "@/context/FavoritesContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { Psychologist } from "@/lib/types";
 import PsychologistCard from "@/components/cards/PsychologistCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { set } from "firebase/database";
+import { fetchFavoritePsychologists } from "@/lib/firebase";
+import Loader from "@/components/ui/Loader";
 
-export default function FavoritesPage() {
+function FavoritesPageContent() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [favoritePsychologists, setFavoritePsychologists] = useState<
@@ -18,14 +21,40 @@ export default function FavoritesPage() {
   const { favorites } = useFavorites();
   const router = useRouter();
 
+  useEffect(() => {
+    async function loadFavorites() {
+      try {
+        setLoading(true);
+        setError(null);
+        if (favorites.length === 0) {
+          setFavoritePsychologists([]);
+          return;
+        }
+        const data = await fetchFavoritePsychologists(favorites);
+        setFavoritePsychologists(data);
+        console.log("Loaded favorite psychologists:", data);
+      } catch (error) {
+        console.error("Error loading favorite psychologists:", error);
+        setError("Failed to load your favorite psychologists");
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadFavorites();
+  }, [favorites]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <section className="max-w-7xl mx-auto px-4 py-8 text-left">
       <h1 className="text-2xl font-semibold mb-4">My Favorites</h1>
       {user ? (
         favorites.length > 0 ? (
           <ul>
-            {favorites.map((psychologist) => (
-              <li key={psychologist.about} className="mb-4">
+            {favoritePsychologists.map((psychologist, index) => (
+              <li key={psychologist.id || index} className="mb-4">
                 <PsychologistCard psychologist={psychologist} />
               </li>
             ))}
@@ -37,5 +66,12 @@ export default function FavoritesPage() {
         <p className="text-gray-500">Please log in to see your favorites</p>
       )}
     </section>
+  );
+}
+export default function FavoritesPage() {
+  return (
+    <ProtectedRoute>
+      <FavoritesPageContent />
+    </ProtectedRoute>
   );
 }
